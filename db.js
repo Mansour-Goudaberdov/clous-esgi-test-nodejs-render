318fbe9d03d14713fd58203b9d5ad20781a13581
GOUDABERDOV <mansourgoudaberdov@gmail.com>
2023-09-26 14:51:58 +0200

    add database config

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ const { Sequelize } = require('sequelize')
 
 // database
 const sequelize = new Sequelize(
-  '', // Database name
-  '', // User
-  '', // Password
+  'cloud_qk5q', // Database name
+  'clouduser', // User
+  '5iDk6dlAFy1x8yvw6oosjzQ6QIVb4p4T', // Password
   {
-    host: '', // Host
+    host: 'dpg-ck9d0fmgtj9c73c4g9e0-a', // Host
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
